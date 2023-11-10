74af2ab5e1b4d99df0f89d9e34bea7b6c29fbf80
IgnatiusChidi <138877827+IgnatiusChidi@users.noreply.github.com>
2023-11-10 16:12:23 +0100

    Update Collections.jsx

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import '../App.css'
-import buffaloRyth from 'src/assets/P2PDWD1.png'
-import checkwave from 'src/assets/P2PDWD2.png'
-import plaidbeet from 'src/assets/P2PDWD3.png'
+import buffaloRyth from './assets/P2PDWD1.png'
+import checkwave from './assets/P2PDWD2.png'
+import plaidbeet from './assets/P2PDWD3.png'
 
 function Collections() {
   return (
